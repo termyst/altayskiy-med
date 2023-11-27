f7994a28f7686d3df02b81aca34be185c69d75da
termyst <131294147+termyst@users.noreply.github.com>
2023-11-27 18:17:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+$(function() {
+     /* Nav Toggle on mobile
+    =================================*/
 
+    let navToggle = $('#navToggle');
+    let nav = $('#nav');
+
+    navToggle.on('click', function(event) {
+        event.preventDefault();
+
+        $(this).toggleClass('active');
+        nav.toggleClass('show');
+        
+        $('body').toggleClass('no-scroll');
+        
+    });
+});
